88f0dadb159fdf0d600c305f7535ba02596c5b66
Craig Curry <craigcurry725@gmail.com>
2026-05-08 00:24:28 -0500

    styled student login page.tsx

--- app/studentlogin/page.tsx
+++ app/studentlogin/page.tsx
@@ -36,41 +36,53 @@ export default function LoginPage() {
   };
 
   return (
-    <main className="p-8">
-      <h1>Student Login</h1>
-      <form onSubmit={handleLogin}>
-        <div>
+    <main className= "min-h-screen flex flex-col items-center justify-start py-10">
+      <h1 className="text-3xl py-2">Student Login</h1>
+      <form onSubmit={handleLogin}
+        className= "max-w-md mx-auto p-6 bg-gray-500 shadow-md py-4"
+        >
+        <div className="flex flex-col space-y-2">
           <label htmlFor="loginName">User Name: </label>
           <input 
+          className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-4 focus:ring-blue-500"
             id="loginName" 
             type="text" 
             value={loginName}
             onChange={(e) => setLoginName(e.target.value)}
-            className="border p-1"
             required 
           />
         </div>
         
-        <div>
+        <div className="flex flex-col space-y-2">
           <label htmlFor="password">Enter Password: </label>
-          <input 
+          <input
+          className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-4 focus:ring-blue-500"
             id="password" 
             type="password" 
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="border p-1"
+            
             required 
           />
         </div>
-
-        <button type="submit">
-          Login
-        </button>
+        <div className="flex justify-center py-4">
+          <button 
+          type="submit"
+          className="px-4 py-2 bg-yellow-600 text-white rounded-md hover:bg-blue-700 transition-colors">
+            Login
+          </button>
+        </div>
+        
       </form>
       
-      <p className="mt-4">
-        If you are not registered click <Link href="/studentregistration">Here</Link>
+      <p className="py-4">If you are not registered click {""}
+          <Link 
+            className="mx-auto px-2 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors italic" 
+            href="/studentregistration">
+              Here
+          </Link>
       </p>
+                
     </main>
   );
 }
